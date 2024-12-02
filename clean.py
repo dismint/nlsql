58ea217be164c1ba48635d1f6f2d789ac7ba9b19
dismint <mintjjc@gmail.com>
2024-12-02 15:34:29 -0500

    done with moira

--- clean.py
+++ clean.py
@@ -1,15 +1,14 @@
 import json
 
-with open('spider_annotated_mapped.json') as f:
+with open('moira_annotated_mapped.json') as f:
     data = json.load(f)
     final = []
     for obj in data:
         newobj = {}
+        newobj["db"] = "dw"
         newobj["question"] = obj["question"]
         newobj["sql"] = obj["sql"]
         newobj["mapping"] = obj["mapping"]
         final.append(newobj)
-    with open('spider_mapped.json', 'w') as outfile:
+    with open('moira_mapped.json', 'w') as outfile:
         json.dump(final, outfile, indent=2)
-
-
